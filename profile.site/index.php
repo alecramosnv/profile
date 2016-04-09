77c6901e955a9e12ddc8ec9efca8dfa1f20ea61c
Alec Ramos <alec1034780@icloud.com>
2016-04-09 16:12:30 -0700

    Move from BitBucket to GitHub

---
+++ profile.site/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Alec R | Web Developer</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="icon" href="images/favicon.ico">
+</head>
+<body>
+	<div id="wrapper">
+
+		<nav>
+			<?php include 'include/logo.php';?>
+		</nav>
+
+		<header></header>
+
+		<section id="social"></section>
+
+		<section id="work"></section>
+
+		<section id="about"></section>
+
+		<footer></footer>
+
+	</div>
+</body>
+</html>
